46a947235731b637a6f4160a65e03959374a9e50
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-26 15:38:44 +0200

    primo commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+let listaCognomi = ['Bianchi','Neri','Rossi','Verdi','Gialli'];
+
+let cognomeInserito = prompt('Inserisci un cognome');
+
+unshift.listaCognomi (cognomeInserito);
+
+console.log (listaCognomi);
